1547653bc4f48c33de8c0d07fe815602b0a6341f
Annie Cai <caishajia@gmail.com>
2023-12-20 14:11:55 -0500

    Time: 55 ms (44.99%) | Memory: 15.4 MB (55.10%) - LeetSync

--- 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.py
+++ 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.py
@@ -4,26 +4,22 @@ class Solution(object):
         :type s: str
         :rtype: str
         """
-        VOWELS = ['a', 'e', 'i', 'o', 'u']
-        string = [c for c in s]
-        print(string)
+        VOWELS = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
+        string = list(s)
         start, end = 0, len(string)-1
 
         while start < end:
-            print(start, end)
-            if string[start].lower() in VOWELS and string[end].lower() in VOWELS:
+            
+            if string[start] not in VOWELS:
+                start += 1
+                
+            if string[end] not in VOWELS:
+                end -= 1
+                
+            if string[start] in VOWELS and string[end] in VOWELS:
                 string[start], string[end] = string[end], s[start]
                 start += 1
                 end -= 1
-                print(start, end)
-            
-            if string[start].lower() not in VOWELS:
-                start += 1
-                print(start, end)
-
-            if string[end].lower() not in VOWELS:
-                end -= 1
-                print(start, end)
-
+               
         return ''.join(string)
         
