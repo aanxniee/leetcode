d3061e10615b461d940e93c90dfcf9bbd84470b9
Annie Cai <caishajia@gmail.com>
2023-12-20 13:29:28 -0500

    Time: 125 ms (24.60%) | Memory: 13.8 MB (62.77%) - LeetSync

---
+++ 605-can-place-flowers/can-place-flowers.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def canPlaceFlowers(self, flowerbed, n):
+        """
+        :type flowerbed: List[int]
+        :type n: int
+        :rtype: bool
+        """
+        
+        c = 0
+
+        for i in range(len(flowerbed)):
+
+            if flowerbed[i] == 0:
+                left = (i == 0) or flowerbed[i-1] == 0
+                right = (i == len(flowerbed) - 1) or flowerbed[i+1] == 0
+                
+                if left and right:
+                    c += 1
+                    flowerbed[i] = 1
+
+        return c >= n
+
+
+
+
+
+        
