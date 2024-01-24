cb3809fb793cf4dd2175991046036441c7502571
Annie Cai <caishajia@gmail.com>
2024-01-24 16:35:07 -0500

    Time: 39 ms (69.94%) | Memory: 17.1 MB (71.32%) - LeetSync

--- 766-flatten-a-multilevel-doubly-linked-list/flatten-a-multilevel-doubly-linked-list.py
+++ 766-flatten-a-multilevel-doubly-linked-list/flatten-a-multilevel-doubly-linked-list.py
@@ -1,48 +1,31 @@
-"""
-# Definition for a Node.
-class Node:
-    def __init__(self, val, prev, next, child):
-        self.val = val
-        self.prev = prev
-        self.next = next
-        self.child = child
-"""
-
-"""
-traverse from head looking for child nodes
-merge a child into the parent list by:
-1. connecting last node in child LL to next node in parent LL
-2. connecting curr node in parent LL to first node in child LL
-"""
 class Solution:
-    def flatten(self, head: 'Optional[Node]') -> 'Optional[Node]':
+    def flatten(self, head: 'Node', next_=None) -> 'Node':
+        
+        curr, prev = head, None
 
-        # function that moves the pointers to merge a child into the LL
-        def merge(curr):
-
-            child = curr.child # get the head of the child LL
-
-            # traverse to the end of the child LL
-            while child.next:
-                child = child.next
-            
-            # link the end of child LL to the next node in the parent LL
-            if curr.next:
-                child.next = curr.next
-                curr.next.prev = child
-
-            # link the current node to the begninning of the child LL
-            curr.next = curr.child
-            curr.child.prev = curr
-
-            curr.child = None # child pointers set to null
-            
-        curr = head
+        # traverse LL
         while curr:
-            if curr.child: # check for child node
-                merge(curr) # if child node, merge into parent LL
+            # has child LL
+            if curr.child:
+            
+                tail = curr.next # the node which the child LL connects to
 
-            curr = curr.next # move to next node
+                # recurse for child LL
+                curr.next = self.flatten(curr.child, tail)
+
+                # linking first node in child LL to node in parent LL
+                if curr.next: 
+                    curr.next.prev = curr
+
+                curr.child = None
+
+            prev = curr
+            curr = curr.next
+
+        # linking last node in child LL to next node in parent LL
+        if prev:  
+            prev.next = next_
+        if next_: 
+            next_.prev = prev
 
         return head
-        
