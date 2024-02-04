5598e7984ef2e7b669af958443863cb45683a3b0
Annie Cai <caishajia@gmail.com>
2024-02-04 13:47:49 -0500

    Time: 54 ms (74.55%) | Memory: 16.6 MB (80.13%) - LeetSync

--- 965-unique-email-addresses/unique-email-addresses.py
+++ 965-unique-email-addresses/unique-email-addresses.py
@@ -2,24 +2,17 @@ class Solution:
     def numUniqueEmails(self, emails: List[str]) -> int:
 
         def formatEmail(email):
-            
+
             local, domain = email.split("@")
-            
-            if "+" in local:
-                local = local.split("+")[0]
+            local = local.split("+")[0].replace(".", "")
 
-            local = local.replace(".", "")
-
-            print(local + "@" + domain)
             return local + "@" + domain
 
-        ans = []
+        ans = set()
 
         for email in emails:
             formattedEmail = formatEmail(email)
-            
-            if formattedEmail not in ans:
-                ans.append(formattedEmail)
+            ans.add(formattedEmail)
 
         return len(ans)
         
