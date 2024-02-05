02d32037a14407474f2d48eea646ac2f162ba6b6
Annie Cai <caishajia@gmail.com>
2024-02-05 14:45:21 -0500

    Time: 83 ms (70.97%) | Memory: 17.9 MB (70.88%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.py
+++ 735-asteroid-collision/asteroid-collision.py
@@ -1,22 +1,27 @@
 class Solution:
+    """
+    need to resolve collisions if:
+    1. stack is non empty
+    2. current asteroid is -ve
+    3. prev asteroid (top of stack) is +ve
+    """
     def asteroidCollision(self, asteroids: List[int]) -> List[int]:
-        stk = []
+        stack = []
 
         for asteroid in asteroids:
-            while stk and stk[-1] > 0 and asteroid < 0:
+            while stack and asteroid < 0 < stack[-1]:
 
-                if stk[-1] == -asteroid:
-                    stk.pop()
-                    break
+                # prev asteroid is smaller, remove +ve asteroid and continue with comparison
+                if stack[-1] < -asteroid:
+                    stack.pop()
+                    continue 
 
-                elif stk[-1] < -asteroid:
-                    stk.pop()
-                    continue
-                    
-                elif stk[-1] > -asteroid:
-                    break
+                # atseroids are equal, destroy both
+                if stack[-1] == -asteroid:
+                    stack.pop()
 
+                break # if prev asteroid is bigger, curr asteroid is destroyed (don't need to do anything)
             else:
-                stk.append(asteroid)
-
-        return stk
+                # current asteroid destroyed all other asteroids
+                stack.append(asteroid)
+        return stack
