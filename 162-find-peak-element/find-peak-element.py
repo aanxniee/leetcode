62e4e4dd8daaf8dba8090e5e3dacc7853512aaab
Annie Cai <caishajia@gmail.com>
2024-02-05 16:00:27 -0500

    Time: 53 ms (29.75%) | Memory: 16.7 MB (79.29%) - LeetSync

--- 162-find-peak-element/find-peak-element.py
+++ 162-find-peak-element/find-peak-element.py
@@ -1,17 +1,22 @@
 class Solution:
+    """
+    you know mid is a peak when nums[mid] > nums[mid+1] and nums[mid-1]
+    """
     def findPeakElement(self, nums: List[int]) -> int:
 
         l, r = 0, len(nums) - 1
-        
+
         while l < r:
-            mid = l +(r-l)//2
-            
-            if nums[mid] < nums[mid+1]:
-                l = mid + 1
-               
-            else:
+
+            mid = (l + r) // 2
+
+            # case: descending slope, peak to left
+            if nums[mid] > nums[mid + 1]:
                 r = mid
-    
+            
+            # case: ascending slope, peak to right
+            else:
+                l = mid + 1
 
         return l
         
